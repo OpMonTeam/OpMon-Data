c0fa01a7c49eebb8c49f138888e504cc76e7bdcc
Navet56 <evandib@gmail.com>
2019-08-03 15:27:05 +0200

    Added the OpMon Tourney Arena (resources)

--- Resources/Map projects/tileset.tsx
+++ Resources/Map projects/tileset.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset name="tileset" tilewidth="32" tileheight="32" tilecount="4096" columns="64">
- <image source="../../data/tileset/tileset.png" width="2048" height="2048"/>
+<tileset version="1.2" tiledversion="1.2.4" name="tileset" tilewidth="32" tileheight="32" tilecount="4096" columns="64">
+ <image source="../../../OpMon/data/tileset/tileset.png" width="2048" height="2048"/>
 </tileset>

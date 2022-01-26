886c68ec953d27c7ce4a006dbb50a19dea774440
Cyrielle Centori <cyriellecentori@protonmail.com>
2022-01-26 12:31:17 +0100

    Added a stop feature for move effects

--- GodotResources/Moves/Effects/ChangeStatEffect.gd
+++ GodotResources/Moves/Effects/ChangeStatEffect.gd
@@ -12,8 +12,9 @@ func _init(p_stat, p_change: int):
 	stat = p_stat
 	change = p_change
 
-func apply(_move, user: OpMon, opponent: OpMon):
+func apply(_move, user: OpMon, opponent: OpMon) -> bool:
 	var target = opponent if to_opponent else user
 	emit_signal("stat_changed", stat, target.change_stat(stat, change))
+	return true
 	
 

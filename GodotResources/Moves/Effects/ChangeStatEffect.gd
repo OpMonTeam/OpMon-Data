a936a4b6d74eb0fc6febdbcb84cca6202a598fd4
Cyrielle Centori <cyriellecentori@protonmail.com>
2022-01-26 10:54:48 +0100

    Added moves

---
+++ GodotResources/Moves/Effects/ChangeStatEffect.gd
@@ -0,0 +1,19 @@
+extends MoveEffect
+
+const Stats = preload("res://Objects/Enumerations.gd").Stats
+
+class_name ChangeStatEffect
+
+export(Stats) var stat
+export(int) var change
+export(bool) var to_opponent
+
+func _init(p_stat, p_change: int):
+	stat = p_stat
+	change = p_change
+
+func apply(_move, user: OpMon, opponent: OpMon):
+	var target = opponent if to_opponent else user
+	emit_signal("stat_changed", stat, target.change_stat(stat, change))
+	
+
